0ca657092a6dc5b410b5a38e992765800e7180e5
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-04-13 23:08:56 -0400

    add fix for cookies

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(
   session({
     secret: process.env.JWT_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_URI,
     }),
